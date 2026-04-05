373f36d0121e36a223aa5e3ea6c20e4b72baf9f4
HienHoang1101 <hienhtt1101@gmail.com>
2026-04-05 22:40:45 +0700

    Create user request

--- frontend/src/Auth0ProviderWithNavigate.tsx
+++ frontend/src/Auth0ProviderWithNavigate.tsx
@@ -1,12 +1,14 @@
 import { Auth0Provider } from "@auth0/auth0-react";
-import type { AppState } from "@auth0/auth0-react";
+import type { AppState, User } from "@auth0/auth0-react";
 import type { PropsWithChildren } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { useCreateUser as userCreateUser } from "./api/MyUserApi";
 
 type Props = PropsWithChildren;
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const { createUser } = userCreateUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -17,8 +19,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     );
   }
 
-  const onRedirectCallback = (appState?: AppState) => {
-    navigate(appState?.returnTo || window.location.pathname);
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,28 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./gobal.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithNavigate from "./Auth0ProviderWithNavigate";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </Router>
+    </QueryClientProvider>
   </StrictMode>,
 );
